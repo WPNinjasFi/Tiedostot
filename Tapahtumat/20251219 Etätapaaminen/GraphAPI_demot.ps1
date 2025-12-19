6fcf97ecb5a46ede926c9501017bc3eb12af62b9
Workplace Ninja User Group Finland <165178018+WPNinjasFi@users.noreply.github.com>
2025-12-19 12:00:57 +0200

    19.12.2025 Etämiitti

---
+++ Tapahtumat/20251219 Etätapaaminen/GraphAPI_demot.ps1
@@ -0,0 +1,70 @@
+﻿Connect-MgGraph -Scopes "DeviceManagementManagedDevices.PrivilegedOperations.All", "Group.ReadWrite.All", "DeviceManagementApps.ReadWrite.All", "DeviceManagementServiceConfig.ReadWrite.All", "DeviceManagementConfiguration.ReadWrite.All", "DeviceManagementManagedDevices.ReadWrite.All", "DeviceManagementRBAC.ReadWrite.All", "Directory.Read.All"
+
+
+### Sync Devices
+$Devices = @()
+$Uri = "https://graph.microsoft.com/v1.0/deviceManagement/managedDevices"
+
+do {
+    $Result = Invoke-MgGraphRequest -Uri $Uri -Method Get -OutputType Json | ConvertFrom-Json
+    $Devices += $Result.value | Where-Object OperatingSystem -eq "Windows" | Select-Object Id, deviceName
+    $Uri = $Result.'@odata.nextLink'
+} while ($Uri)
+
+foreach ($Device in $Devices) {
+    try {
+        $Result = Invoke-MgGraphRequest -Uri "https://graph.microsoft.com/beta/deviceManagement/managedDevices/$($Device.id)/syncDevice" -Method Post -ContentType 'application/json'
+        Write-Host "Success for" $Device.deviceName -Fore Green
+    }
+    catch {
+        Write-Host "Fail for" $Device.deviceName -Fore Red
+    }
+}
+
+### Release all Autopilot devices 
+try {
+    $allDevices = @()
+    $uri = "https://graph.microsoft.com/beta/deviceManagement/windowsAutopilotDeviceIdentities"
+
+    while ($uri) {
+        $Result = Invoke-MgGraphRequest -Uri $uri -Method Get -OutputType Json
+        $json = $Result | ConvertFrom-Json
+
+        $allDevices += $json.value | Where-Object userlessEnrollmentStatus -NE "allowed"
+        $uri = $json.'@odata.nextLink'
+    }
+
+    $Devices = $allDevices
+} catch {
+    Write-Output "error gathering devices"
+}
+
+foreach ($allDevice in $allDevices) {
+    try {
+        $Result = Invoke-MgGraphRequest -Uri "https://graph.microsoft.com/beta/deviceManagement/windowsAutopilotDeviceIdentities/$($allDevice.id)/allowNextEnrollment" -Headers $Headers -Method Post -ContentType 'application/json'
+        Write-Output "Successfully unblocked $($allDevice.id)"
+    }
+    catch {
+        Write-Output "Error unlocking $($allDevice.id)"
+    }
+}
+
+### Rotate Bitlockers
+$Devices = @()
+$Uri = "https://graph.microsoft.com/v1.0/deviceManagement/managedDevices"
+
+do {
+    $Result = Invoke-MgGraphRequest -Uri $Uri -Method Get -OutputType Json | ConvertFrom-Json
+    $Devices += $Result.value | Where-Object OperatingSystem -eq "Windows" | Select-Object Id, deviceName
+    $Uri = $Result.'@odata.nextLink'
+} while ($Uri)
+
+foreach ($Device in $Devices) {
+    try {
+        $Result = Invoke-MgGraphRequest -Uri "https://graph.microsoft.com/beta/deviceManagement/managedDevices/$($Device.id)/rotateBitLockerKeys" -Headers $Headers -Method Post -ContentType 'application/json'
+        write-host "Success for" $Device.deviceName -Fore Green
+    }
+    catch {
+        write-host "Fail for" $Device.devicename -Fore Red
+    }
+}
---
+++ Tapahtumat/20251219 Etätapaaminen/GraphAPI_kirjautumis_demot.ps1
@@ -0,0 +1,28 @@
+﻿### REST
+$body = @{
+    client_id     = ""
+    client_secret = ""
+    scope         = "https://graph.microsoft.com/.default"
+    grant_type    = "client_credentials"
+}
+
+# URL:ssa on tenantin ID, joka pitää vaihtaa
+$url = "https://login.microsoftonline.com/ccef0ddc-6ee0-43c4-809e-2dbdd3703af2/oauth2/v2.0/token"
+
+$token = (Invoke-RestMethod -Uri $url -Method POST -Body $body).access_token
+
+$headers = @{
+    Authorization = "Bearer $token"
+}
+
+$users = (Invoke-RestMethod -Uri "https://graph.microsoft.com/v1.0/users" -Headers $headers).value
+
+### Mg-Graph
+Connect-MgGraph -Scopes "Directory.Read.All"
+$users = Get-MgUser -All
+
+
+### Azure Automation Account / Azure Function APp
+Connect-MgGraph -Identity
+$users = Get-MgUser -All
+
